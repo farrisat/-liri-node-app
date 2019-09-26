0947c6c910af6c012f276661e1c405b554a44bd7
Alex F <farrisat1@gmail.com>
2019-09-25 20:44:40 -0700

    part 2/4ish. spotifyThis completed

--- liri.js
+++ liri.js
@@ -3,10 +3,9 @@ const axios = require("axios");
 var keys = require("./keys.js");
 var moment = require('moment');
 moment().format();
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
 
-
-
+// concertThis function
 function concertThis(artist) {
 var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 axios.get(URL).then(function (response) {
@@ -22,4 +21,21 @@ axios.get(URL).then(function (response) {
     console.log(error);
   });
 }
-concertThis("taylor swift");
+
+
+//spotifyThis function
+function spotifyThis(songName){
+var spotify = new Spotify(keys.spotify)
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    data.tracks.items[0].artists.forEach(element => {
+        console.log(element.name)
+    });
+  console.log(data.tracks.items[0].name);
+  console.log(data.tracks.items[0].external_urls.spotify);
+  console.log(data.tracks.items[0].album.name)
+  console.log("\n")
+});
+}
